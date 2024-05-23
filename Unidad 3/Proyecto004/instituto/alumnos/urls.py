7bbd61c6e1977e82815e4fb97582ac3169fec9c9
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-05-23 09:53:21 -0400

    update

---
+++ Unidad 3/Proyecto004/instituto/alumnos/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+#http://127.0.0.1:8000/alumnos/index
+urlpatterns = [
+    path('index', views.index , name='index'),
+#    path('listarAlumnos', views.listarAlumnos , name='listarAlumnos'),
+]
