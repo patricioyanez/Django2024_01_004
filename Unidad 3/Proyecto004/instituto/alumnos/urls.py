93a01a5c87ab577bc24d9624b99f2773ac4932f2
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-05-31 12:03:25 -0400

    update crud

--- Unidad 3/Proyecto004/instituto/alumnos/urls.py
+++ Unidad 3/Proyecto004/instituto/alumnos/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 #http://127.0.0.1:8000/alumnos/index
 urlpatterns = [
+    path('', views.menu , name='menu'),
     path('index', views.index , name='index'),
     path('listarAlumnos', views.listarAlumnos , name='listarAlumnos'),
+    path('listarEscuela', views.listarEscuela , name='listarEscuela'),
+    path('listarCarrera', views.listarCarrera , name='listarCarrera'),
+    path('GuardarCarrera', views.GuardarCarrera , name='GuardarCarrera'),
 ]
--- Unidad 3/Proyecto004/instituto/alumnos/views.py
+++ Unidad 3/Proyecto004/instituto/alumnos/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from .models import Alumno
+from .models import Alumno,Escuela,Carrera
 # Create your views here.
+def menu(request):
+    context = {}
+    return render(request, 'plantillaBase.html', context)
 def index(request):
     context = {}
     return render(request, 'index.html', context)
@@ -9,7 +12,25 @@ def listarAlumnos(request):
     # utilizar ORM de Django
     # select * from alumno
     alumnos = Alumno.objects.all()
-
     context = {'alumnos': alumnos}
-
     return render(request, 'listarAlumno.html', context)
+
+
+def listarEscuela(request):
+    # utilizar ORM de Django
+    # select * from alumno
+    listado = Escuela.objects.all()
+    context = {'listado': listado}
+    return render(request, 'listarEscuela.html', context)
+
+def listarCarrera(request):
+    # utilizar ORM de Django
+    # select * from alumno
+    listado = Carrera.objects.all()
+    context = {'listado': listado}
+    return render(request, 'listarCarrera.html', context)
+
+def GuardarCarrera(request):
+    escuelas = Escuela.objects.all()
+    context = {'escuelas':escuelas}
+    return render(request, 'GuardarCarrera.html', context)
